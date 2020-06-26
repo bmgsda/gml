dea55f01a024fd9d1d06e2d889f64530fd8bd4a9
bmgsda <59072657+bmgsda@users.noreply.github.com>
2020-06-26 01:00:57 -0300

    Add files via upload

---
+++ Principal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CIV_CommonValidators
+{
+    public class LayoutValidators
+    {
+        private String AjustarNumeroCaracteres(String baseString, int numCaracters)
+        {
+            if (baseString.Length > numCaracters)
+            {
+                return $"A informação enviada ultrapassa {numCaracters} dígitos";
+            }
+            else
+            {
+                while (baseString.Length < numCaracters)
+                {
+                    baseString = '0' + baseString;
+                }
+                return baseString;
+            }
+        }
+        public String AjustarNumeroContratoVeiculos(String numContrato)
+        {
+            return AjustarNumeroCaracteres(numContrato, 8);
+        }
+
+        public String AjustarCPF(String numCpf)
+        {
+            return AjustarNumeroCaracteres(numCpf, 11);
+        }
+
+        public String CapitalizarString(String baseString)
+        {
+            return baseString.ToUpper();
+        }
+
+    }
+}
